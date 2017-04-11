306e4548e7bc7bd86e57247821747906b840ad41
maxencej <maxence.joncheray@gmail.com>
2017-04-11 15:21:31 +0200

    add query optimisation n+1

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
-    @articles = Article.all
+    @articles = Article.includes(:image, :user, :category)
   end
 
   def show
